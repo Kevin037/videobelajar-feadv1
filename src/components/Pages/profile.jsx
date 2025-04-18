dfe669eedd39b209c527f5697cc88dbb9b6175ef
Kevin Satria Muhammad Iqbal <kevin@smart-it.co.id>
2025-04-18 08:11:32 +0700

    Integrasi Update Foto Profile

--- src/components/Pages/profile.jsx
+++ src/components/Pages/profile.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Authlayout from "../Layouts/AuthLayout";
 import { Card } from "../Elements/card";
 import { H2 } from "../Elements/heading";
@@ -6,15 +6,18 @@ import { SidebarMenu } from "../Fragments/SidebarMenu";
 import { ButtonPrimaryMDSubmit } from "../Elements/button";
 import { FloatingInput, Select } from "../Elements/input";
 import useUser from "../../hooks/useUser";
+import Resizer from 'react-image-file-resizer';
 
 const token = localStorage.getItem("token");
 const auth = localStorage.getItem("user");
 const ProfilePage = () => {
-
+    const [file, setFile] = useState(null);
+    const [base64photo, setBase64photo] = useState('');
     const { currentUser } = useUser(auth);
     const [name, setName] = useState("");
     const [no_hp, setNoHp] = useState("");
     const { update, status } = useUser();
+    const selectPhotoRef = useRef();
 
     useEffect(() => {
         if(token === null) {
@@ -39,6 +42,48 @@ const ProfilePage = () => {
             window.location.reload();   
         }
     },[status])
+   
+    const selectPhoto = () => {
+        selectPhotoRef.current.click();
+      }
+  
+      const handleFileChange = (e) => {
+        const selectedFile = e.target.files[0];
+        setFile(selectedFile);
+      };
+  
+      useEffect(() => {
+        if (file != null) {
+            ResizePhoto(file)   
+        }
+      },[file])
+  
+      const ResizePhoto = (file) => {
+        new Promise(() => {
+            Resizer.imageFileResizer(
+              file,
+              300,
+              300,
+              'JPEG',
+              100,
+              0,
+              (uri) => {
+                setBase64photo(uri);
+              },
+              'base64',
+            );
+        });
+    };
+  
+    useEffect(() => {
+      if (base64photo != "") {
+          UpdatePhotoDB();
+      }
+  },[base64photo]); 
+  
+    const UpdatePhotoDB = async () => {
+        update(auth,{ photo:base64photo });
+    }
  return (
     <Authlayout title="Home" navType="home" withFooter={true}>
         <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
@@ -52,17 +97,29 @@ const ProfilePage = () => {
                     <Card>
                         <div className="grid grid-cols-12 ... gap-3">
                             <div className="col-span-3 md:col-span-2 ...">
-                                <img  
+                            {currentUser?.photo != null ? (
+                                <img
                                 className="object-cover h-auto rounded-lg"
-                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" 
+                                src={currentUser?.photo} 
                                 alt="" />
+                            ) : (
+                                <img
+                                className="object-cover h-auto rounded-lg"
+                                src="https://github.com/shadcn.png" 
+                                alt="" />
+                            )}
                             </div>
+                            <input type="file" 
+                          ref={selectPhotoRef} 
+                          className="hidden"
+                          onChange={handleFileChange}  
+                          name="" id="" />
                             <div className="col-span-9 md:col-span-10 ...">
                                 <div className="text-sm">
                                     <H2>{currentUser?.name}</H2>
                                     <p>{currentUser?.email}</p>
                                 </div>
-                                <button className="mt-3 text-red-600 text-sm cursor-pointer">Ganti Foto Profil</button>
+                                <button onClick={() => selectPhoto()} className="mt-3 text-red-600 text-sm cursor-pointer">Ganti Foto Profil</button>
                             </div>
                         </div>
                         <hr className="mt-6 text-slate-200" />
